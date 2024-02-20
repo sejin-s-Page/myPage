cb38bbe937db1487d40e92c4793c98467abb116c
sejin <sejinjiji@gmail.com>
2024-02-20 13:14:01 +0900

    Start Django Project

---
+++ mypage_prj/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mypage_prj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mypage_prj.settings")
+
+application = get_asgi_application()
