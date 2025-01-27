76cd3a1694872dad55a6982d659ba9f044b5f622
sejin <sejinjiji@gmail.com>
2025-01-28 00:45:57 +0900

    Deploy 배포 이슈 해결을 위해 커밋

--- manage.py
+++ manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
+import django.conf
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myPagePrj.settings.dev')
+    try:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myPagePrj.settings.prod')
+    except:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myPagePrj.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
