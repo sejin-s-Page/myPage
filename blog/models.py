aee7874cc29d4b66effd5cabc2621134e84b1115
sejin <sejinjiji@gmail.com>
2025-01-27 21:38:04 +0900

    Feat blog 목록페이지 작성 (#7)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    content = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from blog.models import Post
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog app index.")
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
