1cc07eb90aa4f720733a03059bde1e14abddb4ef
sejin <sejinjiji@gmail.com>
2025-02-07 04:06:45 +0900

    Feat blog detailview 작성

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from gc import get_objects
+
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
@@ -6,3 +8,8 @@ from blog.models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
