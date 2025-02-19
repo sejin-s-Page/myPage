26cc12c17e61985f497ba96d5852fa5d6433c455
sejin <sejinjiji@gmail.com>
2025-02-19 12:39:32 +0900

    Feat 포스트 작성 페이지 (#20)

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=100)
-    content = models.TextField()
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+    content = CKEditor5Field('content', config_name='extends')
     upload_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,27 @@
 from gc import get_objects
-
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
-
+from blog.forms import BlogPost
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-id')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# @login_required
+def post_create(request):
+    if request.method == 'POST':
+        post_form = BlogPost(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('post-list')
+    else:
+        post_form = BlogPost()
+        return render(request, 'blog/post_create.html', {'form': post_form})
